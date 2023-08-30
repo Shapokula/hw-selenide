2e48c2c64230ddd33ee0cb0bdc2301569e9e2daf
Dora Charm <kovina.dora@gmail.com>
2023-08-30 20:01:00 +0500

    Fixes

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -1,17 +1,11 @@
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Calendar;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 

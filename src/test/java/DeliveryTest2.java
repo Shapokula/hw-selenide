1798d16a13496f30880c829d5ae06435413fb120
Dora Charm <kovina.dora@gmail.com>
2023-08-31 17:02:03 +0500

    Fixes

--- src/test/java/DeliveryTest2.java
+++ src/test/java/DeliveryTest2.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -20,13 +21,17 @@ public class DeliveryTest2 {
         $("[data-test-id='city'] input").setValue("Че");
         $$(".menu-item").find(text("Челябинск")).click();
         $("[data-test-id='date']").click();
-        if (!generateDate(7, "MM").equals(generateDate(3, "MM"))) {
+        int daysToAdd = 7;
+        String planningDate = generateDate(daysToAdd, "dd.MM.yyyy");
+        if (!generateDate(daysToAdd, "MM").equals(generateDate(3, "MM"))) {
         $("[data-step='1']").click(); }
-        $$("[data-day]").findBy(text(generateDate(7, "d"))).click();
+        $$("[data-day]").findBy(text(generateDate(daysToAdd, "d"))).click();
         $("[data-test-id='name'] input").setValue("Кузнецова Александра");
         $("[data-test-id='phone'] input").setValue("+79820000000");
         $("[data-test-id='agreement']").click();
         $(".button").click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
